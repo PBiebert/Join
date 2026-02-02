e06dd407b563139165e55597b970ffb8674e9d64
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2026-02-02 21:01:23 +0100

    feat(imprint): Legal Notice Seite implementiert, Responsive Layout (Desktop + Mobile)

--- src/app/pages/imprint/imprint.ts
+++ src/app/pages/imprint/imprint.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Nav } from '../../shared/components/nav/nav';
+import { Header } from '../../shared/components/header/header';
 
+/**
+ * Legal Notice (Imprint) page component
+ * @description Displays legal information, contact details and terms of use
+ */
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  standalone: true,
+  imports: [Nav, Header],
   templateUrl: './imprint.html',
-  styleUrl: './imprint.scss',
+  styleUrl: './imprint.scss'
 })
 export class Imprint {
+  constructor(private location: Location) {}
 
+  /**
+   * Navigates back to the previous page
+   */
+  goBack(): void {
+    this.location.back();
+  }
 }
