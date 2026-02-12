636384c46b7a716544fe7dcff6f5671136c16dd5
Philipp Biebert <philipp.biebert@gmail.com>
2026-02-12 17:12:45 +0100

    feat: implement add-task component in dialog

--- src/app/pages/board/add-task-dialog/add-task-dialog.ts
+++ src/app/pages/board/add-task-dialog/add-task-dialog.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TasksService } from '../../../services/tasks-service';
+import { AddTask } from '../../add-task/add-task';
 
 @Component({
   selector: 'app-add-task-dialog',
-  imports: [],
+  imports: [AddTask],
   templateUrl: './add-task-dialog.html',
   styleUrl: './add-task-dialog.scss',
 })
-export class AddTaskDialog {
+export class AddTaskDialog implements AfterViewInit, OnDestroy {
+  tasksService = inject(TasksService);
+  private taskSub!: Subscription;
+  @ViewChild('dialog') addTaskDialog!: ElementRef;
 
+  ngAfterViewInit(): void {
+    this.taskSub = this.tasksService.openAddTaskDialog$.subscribe((open) => {
+      if (open) {
+        this.openDialog();
+      } else {
+        this.closeDialog();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.taskSub?.unsubscribe();
+  }
+
+  openDialog() {
+    this.addTaskDialog.nativeElement.showModal();
+  }
+
+  closeDialog() {
+    this.addTaskDialog.nativeElement.close();
+  }
 }
