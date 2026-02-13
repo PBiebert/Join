71326e2978eff2638a97f3c682583576ec485b51
Philipp Biebert <philipp.biebert@gmail.com>
2026-02-13 10:56:51 +0100

    feat: hide app-header and app-nav in add-task component when dialog is open

--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../../shared/components/nav/nav';
 import { Header } from '../../shared/components/header/header';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
+import { TasksService } from '../../services/tasks-service';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [Nav, Header, CommonModule],
+  imports: [Nav, Header, CommonModule, NgClass],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
 export class AddTask {
-
+  tasksService = inject(TasksService);
   // Date only for today and future
   minDate: string = new Date().toISOString().split('T')[0];
 
@@ -20,11 +21,7 @@ export class AddTask {
   isOpen = false;
   selectedOption: string = 'Select contacts to assign';
 
-  options: string[] = [
-    'Option_1',
-    'Option_2',
-    'Option_3'
-  ];
+  options: string[] = ['Option_1', 'Option_2', 'Option_3'];
 
   toggleDropdown() {
     this.isOpen = !this.isOpen;
@@ -44,10 +41,7 @@ export class AddTask {
   isCategoryOpen = false;
   selectedCategory: string = 'Select category';
 
-  categoryOptions: string[] = [
-    'Task',
-    'Subtask'
-  ];
+  categoryOptions: string[] = ['Task', 'Subtask'];
 
   toggleCategoryDropdown() {
     this.isCategoryOpen = !this.isCategoryOpen;
